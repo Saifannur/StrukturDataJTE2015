26cdc7a5c8619e7224482188b03fe92482657fdb
Misrizal Saifannur <saif4nnur@gmail.com>
2015-10-10 16:37:10 +0700

    Kuis-02-Rekam-Medik

---
+++ Kuis-02-Rekam-Medik/Dokter.java
@@ -0,0 +1,12 @@
+
+
+public class Dokter{
+    
+    String nama;
+    Pasien [] pasien;
+    public Dokter (String nama, Pasien [] pasien){
+        this.nama=nama;
+        this.pasien=pasien;
+    }
+  
+}
